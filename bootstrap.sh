e523d6202976efa305322df36f09543dd32006aa
vaibhavgupta3110 <vaibhav.gupta@me.com>
2017-03-24 05:22:09 +0530

    more

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,12 +11,12 @@ sudo apt-get -y autoremove
 # install usuals
 sudo apt-get -y -f install linux-headers-"$(uname -r)" \
   git g++ libssl-dev libncurses5-dev bc m4 make unzip libmnl-dev libssh-dev \
-  bison cmake automake autoconf build-essential libpq-dev libffi-dev clang \
+  bison cmake automake autoconf build-essential libpq-dev libffi-dev clang ssh \
   curl wget libtool python python-pip cpio bzip2 gcc python3-ply ncurses-dev \
   python-yaml graphviz python-apt openssl fop xsltproc unixodbc-dev \
   python3-apt arduino gcc-avr avr-libc avrdude arduino-core arduino-mk \
   python-configobj python-jinja2 python-serial default-jdk squashfs-tools \
-  ssh-askpass software-properties-common
+  ssh-askpass software-properties-common libicu-dev libmozjs-dev
 
 # update package sources & keys
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
@@ -33,3 +33,19 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 gpg --keyserver hkp://keys.gnupg.net --recv-keys \
   409B6B1796C275462A1703113804BB82D39DC0E3
+
+# housekeeping
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install rvm
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+
+#install couchdb repos
+sudo apt-add-repository -y ppa:couchdb/stable
+sudo apt-get update
+sudo apt-get -y -f remove couchdb couchdb-bin couchdb-common
+sudo apt-get install -y -f couchdb
+  # test couchdb installation
+curl localhost:5984
